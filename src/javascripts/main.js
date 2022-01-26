033dd95261ae23a8cb09b7d9c328e30843194777
Altairyu <82081949+altairyu@users.noreply.github.com>
2022-01-26 16:27:54 +0900

    Clean up project

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,2 @@
 import './reactApp.jsx';
-import my from "./my.js";
 import '../stylesheets/main.scss';
-
-
-import add from './add.ts';
-
-
-console.log(add(4,9));
-console.log('webpack!');
-my();
